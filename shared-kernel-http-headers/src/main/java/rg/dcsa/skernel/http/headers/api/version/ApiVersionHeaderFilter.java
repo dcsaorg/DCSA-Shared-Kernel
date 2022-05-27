f310a4cf5a17008bf85273b45196e70a15dc55c5
Finn Nielsen <fn@asseco.dk>
2022-05-27 15:26:20 +0200

    DDT-1079 - Modulize it

---
+++ shared-kernel-http-headers/src/main/java/rg/dcsa/skernel/http/headers/api/version/ApiVersionHeaderFilter.java
@@ -0,0 +1,23 @@
+package rg.dcsa.skernel.http.headers.api.version;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ApiVersionHeaderFilter extends OncePerRequestFilter {
+  @Value("${dcsa.specification.version:N/A}")
+  private String apiVersion;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    response.setHeader("API-Version", apiVersion);
+    filterChain.doFilter(request, response);
+  }
+}
