9df2cbf7590435416c44b770551ba541a979726d
abf <abf@asseco.dk>
2022-05-30 09:46:25 +0200

    DDT-1078: migrate validators

---
+++ shared-kernel-infrastructure/src/main/java/org/dcsa/skernel/infrastructure/validation/EnumSubset.java
@@ -0,0 +1,47 @@
+package org.dcsa.skernel.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Enforce that a given field only uses a particular subset of an enum
+ *
+ * Example usage:
+ *
+ * <pre>{@code
+ * @Data
+ * public class FooProgressUpdate {
+ *
+ *     // In a progress update, the "CREATED" status code is not permitted.
+ *     @EnumSubset(anyOf = "STARTED,PENDING_FEEDBACK,FINISHED")
+ *     private FooStatusTypeCode fooStatusTypeCode;
+ * }
+ *
+ * public enum FooStatusTypeCode {
+ *     CREATED,
+ *     STARTED,
+ *     PENDING_FEEDBACK,
+ *     FINISHED;
+ * }
+ *
+ * }</pre>
+ */
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = EnumSubsetValidator.class)
+public @interface EnumSubset {
+  String[] anyOf();
+
+  String message() default "must be any of {anyOf}";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+}
---
+++ shared-kernel-infrastructure/src/main/java/org/dcsa/skernel/infrastructure/validation/EnumSubsetValidator.java
@@ -0,0 +1,56 @@
+package org.dcsa.skernel.validator;
+
+import org.apache.commons.lang3.NotImplementedException;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Validator for Enumeration subsets. Works both for queryParameters (where the value to validate is
+ * a string) and for Enum defined fields (where the value to validate is an Enum)
+ *
+ * <p>If the value is a string - then this class supports that the string can be comma separated in
+ * order to validate a set of values
+ */
+public class EnumSubsetValidator implements ConstraintValidator<EnumSubset, Object> {
+
+  private Set<String> subsetTypes;
+
+  @Override
+  public void initialize(EnumSubset constraintAnnotation) {
+    this.subsetTypes = Arrays.stream(constraintAnnotation.anyOf())
+      .map(s -> s.split(","))
+      .flatMap(Arrays::stream)
+      .collect(Collectors.toSet());
+  }
+
+  @Override
+  public boolean isValid(Object types, ConstraintValidatorContext constraintValidatorContext) {
+    if (null == types) {
+      return true;
+    }
+
+    if (types instanceof String) {
+      Stream<String> enumStream =
+        ((String) types).contains(",")
+          ? Arrays.stream(((String) types).split(","))
+          : Stream.of((String) types);
+      return enumStream.allMatch(e -> subsetTypes.contains(e));
+    } else if (types instanceof List) {
+      @SuppressWarnings("unchecked")
+      List<? extends Enum<?>> enumList = (List<? extends Enum<?>>) types;
+      return enumList.stream()
+        .map(Enum::name)
+        .allMatch(subsetTypes::contains);
+    } else if (types instanceof Enum) {
+      return subsetTypes.contains(((Enum<?>) types).name());
+    } else {
+      throw new NotImplementedException("type not implemented:" + types.getClass().getTypeName());
+    }
+  }
+}
