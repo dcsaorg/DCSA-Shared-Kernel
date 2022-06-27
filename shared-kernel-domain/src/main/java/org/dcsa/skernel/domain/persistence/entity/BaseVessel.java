3d808969f7a7b0d0408ad9281c890dfb571000e7
Frazer Joe Dourado <frazer.dourado@dcsa.org>
2022-06-27 10:20:14 +0200

    Moved vessel & carrier to sk.

---
+++ shared-kernel-domain/src/main/java/org/dcsa/skernel/domain/persistence/entity/BaseVessel.java
@@ -0,0 +1,63 @@
+package org.dcsa.skernel.domain.persistence.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+@MappedSuperclass
+@Setter(value = AccessLevel.PROTECTED)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vessel")
+public class BaseVessel {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false)
+  protected UUID id;
+
+  @Column(name = "vessel_imo_number", length = 7, unique = true)
+  protected String vesselIMONumber;
+
+  @Column(name = "vessel_name", length = 35)
+  protected String vesselName;
+
+  @Column(name = "vessel_flag", length = 2, columnDefinition = "bpchar")
+  protected String vesselFlag;
+
+  @Column(name = "vessel_call_sign", length = 18)
+  protected String vesselCallSign;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "vessel_operator_carrier_id")
+  protected Carrier vesselOperatorCarrier;
+
+  @Column(name = "is_dummy")
+  protected Boolean isDummyVessel;
+
+  @Column(name = "length", columnDefinition = "numeric")
+  protected Float vesselLength;
+
+  @Column(name = "width", columnDefinition = "numeric")
+  protected Float vesselWidth;
+
+  @Column(name = "dimension_unit", length = 3)
+  protected String dimensionUnit;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+    BaseVessel vessel = (BaseVessel) o;
+    return id != null && Objects.equals(id, vessel.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
+  }
+}
