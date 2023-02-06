2b439f2f5fcad7885f994dc057000bbcb6e296cd
Finn Nielsen <finn.nielsen@dcsa.org>
2023-02-06 12:12:49 +0100

    DDT-1532

--- shared-kernel-infrastructure/src/main/java/org/dcsa/skernel/infrastructure/validation/ISO6346EquipmentReference.java
+++ shared-kernel-infrastructure/src/main/java/org/dcsa/skernel/infrastructure/validation/ISO6346EquipmentReference.java
@@ -1,7 +1,24 @@
 package org.dcsa.skernel.infrastructure.validation;
 
+import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = ISO6346EquipmentReferenceValidator.class)
 public @interface ISO6346EquipmentReference {
 
   String message() default "must be a valid ISO-6346 equipment reference";
