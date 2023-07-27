3521f5f63b9cdbb679b81b0b7f27171288139a11
Niels Thykier <nit@globeteam.com>
2023-07-27 12:19:07 +0200

    Make the AddressMapper strict in target mapping
    With this change, it is now a compile error if the AddressMapper
    cannot map an attribute in the target entity. So we have to declare
    `id` as explicitly not mapped.
    
    With this, EDocumentation can do strict mapping for the majority of
    its own entities.
    
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- shared-kernel-infrastructure-services/src/main/java/org/dcsa/skernel/infrastructure/services/mapping/AddressMapper.java
+++ shared-kernel-infrastructure-services/src/main/java/org/dcsa/skernel/infrastructure/services/mapping/AddressMapper.java
@@ -3,9 +3,12 @@ package org.dcsa.skernel.infrastructure.services.mapping;
 import org.dcsa.skernel.domain.persistence.entity.Address;
 import org.dcsa.skernel.infrastructure.transferobject.AddressTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface AddressMapper {
   AddressTO toDTO(Address address);
+  @Mapping(target = "id", ignore = true)
   Address toDAO(AddressTO address);
 }
